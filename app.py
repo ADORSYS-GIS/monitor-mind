124652b84fd9547a5afca1049fd7d0bd6385a96d
T.Arthur <tcheutchouaarthur38@gmail.com>
2023-11-10 14:44:06 +0100

    modification of ram and swap usage

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from flask_apscheduler import APScheduler
 # Import service modules
 import services.cpu_service as cpu_service
 import services.memory_service as memory_service
-
+import services.ram_and_swap_usage as ram_and_swap_usage
 
 # Add other necessary imports here
 
@@ -43,34 +43,12 @@ def get_memory():
     """API endpoint to get memory (RAM and Swap) usage."""
     ram_usage, swap_usage = memory_service.get_memory_usage()
     return jsonify(ram_usage_history=ram_usage, swap_usage_history=swap_usage)
-
-
+    ram_and_swap_usage
 # Add other API endpoints for additional resources
 
 @scheduler.task('interval', id='cpu_get_data', seconds=15, misfire_grace_time=1000)
 def actualise_cpu_data():
     cpu_service.calculate_cpu_usage()
-import psutil
-
-# Get the RAM usage
-ram = psutil.virtual_memory()
-ram_total = ram.total
-ram_used = ram.used
-ram_percent = ram.percent
-
-print(f"RAM Total: {ram_total} bytes")
-print(f"RAM Used: {ram_used} bytes")
-print(f"RAM Percent: {ram_percent}%")
-
-# Get the SWAP usage
-swap = psutil.swap_memory()
-swap_total = swap.total
-swap_used = swap.used
-swap_percent = swap.percent
-
-print(f"SWAP Total: {swap_total} bytes")
-print(f"SWAP Used: {swap_used} bytes")
-print(f"SWAP Percent: {swap_percent}%")
 
 if __name__ == '__main__':
     app.run(debug=True, port=2376)
