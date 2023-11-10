d14a6be58f33b8e90e6666528b952082b8d47aef
T.Arthur <tcheutchouaarthur38@gmail.com>
2023-11-10 21:57:38 +0100

    add dashboard ram and swap display

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_apscheduler import APScheduler
 # Import service modules
 import services.cpu_service as cpu_service
 import services.memory_service as memory_service
-import services.ram_and_swap_usage as ram_and_swap_usage
 
 # Add other necessary imports here
 
@@ -37,13 +36,13 @@ def get_cpu():
     timestamps, cpu_usage = cpu_service.get_cpu_usage_array()
     return jsonify(timestamps=timestamps, cpu_usage=cpu_usage)
 
-
 @app.route('/api/memory')
 def get_memory():
     """API endpoint to get memory (RAM and Swap) usage."""
-    ram_usage, swap_usage = memory_service.get_memory_usage()
-    return jsonify(ram_usage_history=ram_usage, swap_usage_history=swap_usage)
-    ram_and_swap_usage
+    ram_timestamps, ram_usage = memory_service.get_ram_usage_array()
+    swap_timestamps, swap_usage = memory_service.get_swap_usage_array()
+    return jsonify(ram_timestamps=ram_timestamps, ram_usage_history=ram_usage,swap_timestamps=swap_timestamps, swap_usage_history=swap_usage)
+
 # Add other API endpoints for additional resources
 
 @scheduler.task('interval', id='cpu_get_data', seconds=15, misfire_grace_time=1000)
--- services/memory_service.py
+++ services/memory_service.py
@@ -1,4 +1,33 @@
+import psutil
+import services.time_service as ts
+
+fake_ram_mem = {}
+fake_swap_mem = {}
+
 def get_memory_usage():
     """Get the current memory (RAM and Swap) usage as a percentage."""
-    return [], []
+    ram_usage = psutil.virtual_memory()
+    swap_usage = psutil.swap_memory()
 
+    ram_timestamp = ts.get_history_time()
+    swap_timestamp = ts.get_history_time()
+
+    return ram_timestamp, ram_usage, swap_timestamp, swap_usage
+
+def get_ram_usage_array():
+    timestamps = [i for i in fake_ram_mem.keys()]
+    ram_usage = [i for i in fake_ram_mem.values()]
+    return timestamps, ram_usage
+
+def calculate_ram_usage():
+    timestamp, _, _, ram_usage = get_memory_usage()
+    fake_ram_mem[timestamp] = ram_usage
+
+def get_swap_usage_array():
+    timestamps = [i for i in fake_swap_mem.keys()]
+    swap_usage = [i for i in fake_swap_mem.values()]
+    return timestamps, swap_usage
+
+def calculate_swap_usage():
+    timestamp, _, _, _, swap_usage = get_memory_usage()
+    fake_swap_mem[timestamp] = swap_usage
