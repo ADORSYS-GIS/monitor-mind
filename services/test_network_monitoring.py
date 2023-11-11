ef7008c82c03275b99242397d331ab1ccb40e380
kouamschekina <kouamdelphine82@gmail.com>
2023-11-11 20:53:29 +0100

    test to validate network monitoring

---
+++ services/test_network_monitoring.py
@@ -0,0 +1,10 @@
+from network_service import get_network_usage as gnu
+import unittest
+
+class NetworkMonitoringTest(unittest.TestCase):
+
+    def test_network_monitoring(self):
+        result = gnu
+        self.assertTrue(result)
+if __name__ == '__main__':
+    unittest.main()
